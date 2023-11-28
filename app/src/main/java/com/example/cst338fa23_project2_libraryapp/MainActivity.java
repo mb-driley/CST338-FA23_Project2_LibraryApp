a5e8d035c5b2ab112c8c37c99cbab44ef181eefe
drile <driley@csumb.edu>
2023-11-28 09:00:42 -0800

    Implemented wireUpDisplay() & getValuesFromDisplay() in MainActivity.java Implemented Fields: mUsernameField (EditText), mPasswordField (EditText), mButton (Button), mUsername (String), mPassword (String)

--- app/src/main/java/com/example/cst338fa23_project2_libraryapp/MainActivity.java
+++ app/src/main/java/com/example/cst338fa23_project2_libraryapp/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.cst338fa23_project2_libraryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mUsernameField, mPasswordField;
+    private Button mButton;
+    private String mUsername, mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void wireUpDisplay() {
+        mUsernameField = findViewById(R.id.editTextLoginUserName);
+        mPasswordField = findViewById(R.id.editTextLoginPassword);
+        mButton = findViewById(R.id.buttonLogin);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getValuesFromDisplay();
+            }
+        });
+    }
+
+
+    private void getValuesFromDisplay() {
+        mUsername = mUsernameField.getText().toString();
+        mPassword = mPasswordField.getText().toString();
     }
 }
