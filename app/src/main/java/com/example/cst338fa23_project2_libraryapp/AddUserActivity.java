0ba012aec464f9b8cb2cc90a7cfdfb6171273f53
drile <driley@csumb.edu>
2023-12-01 09:38:23 -0800

    Updated AddUserActivity.java Implemented fields: mReturnButton, mCreateAccountButton, mUsernameField (EditText), mPasswordField (EditText) Implemented wireUpDisplay() method

--- app/src/main/java/com/example/cst338fa23_project2_libraryapp/AddUserActivity.java
+++ app/src/main/java/com/example/cst338fa23_project2_libraryapp/AddUserActivity.java
@@ -3,12 +3,28 @@ package com.example.cst338fa23_project2_libraryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddUserActivity extends AppCompatActivity {
+
+    private Button mReturnButton, mCreateAccountButton;
+    private EditText mUsernameField, mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+        wireUpDisplay();
+
+
+    }
+
+    private void wireUpDisplay() {
+        mReturnButton = findViewById(R.id.addUserReturnButton);
+        mCreateAccountButton = findViewById(R.id.addUserCreateButton);
+        mUsernameField = findViewById(R.id.addUserUsernameEditText);
+        mPasswordField = findViewById(R.id.addUserPasswordEditText);
     }
 }
